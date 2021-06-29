c9afd38ecfa35008373d8d356e3f9c1be3c38826
inthemonzon <inthemonzon@yahoo.com>
2021-06-29 14:09:07 -0400

    commit tree b664da026abc8acedd44feace539df5ca8d55bab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!` 
+}
+
+function mondayWork(mon = "go to the office") {
+    return `This Monday, I will ${mon}.`
+}
+function wrapAdjective(flair = "*") {
+    return function innerFunction(inner = "special") {
+        return `You are ${flair + inner + flair}!`
+    }
+}
